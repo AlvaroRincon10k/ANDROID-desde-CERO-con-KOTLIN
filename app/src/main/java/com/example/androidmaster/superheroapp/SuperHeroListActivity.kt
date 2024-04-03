afabc7ed67ef68afd40714f236a2ae75a1f1055c
Alvaro Rincon <alvarorinconc1@gmail.com>
2024-04-02 20:45:17 -0500

    Retrofit.
    - Se añaden las dependencias necesarias para trabajar con retrofit.
    - Se crea el metodo "getRetrofit" para la instancia del retrofit y se añade la URL base.

--- app/src/main/java/com/example/androidmaster/superheroapp/SuperHeroListActivity.kt
+++ app/src/main/java/com/example/androidmaster/superheroapp/SuperHeroListActivity.kt
@@ -6,15 +6,19 @@ import android.os.Bundle
 import androidx.appcompat.widget.SearchView
 import com.example.androidmaster.R
 import com.example.androidmaster.databinding.ActivitySuperHeroListBinding
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class SuperHeroListActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySuperHeroListBinding
+    private lateinit var retrofit: Retrofit
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySuperHeroListBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        retrofit = getRetrofit()
         initUI()
     }
 
@@ -31,5 +35,12 @@ class SuperHeroListActivity : AppCompatActivity() {
 
     private fun searchByname(query: String) {
 
+    }
+
+    private fun getRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("https://superheroapi.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
     }
 }
